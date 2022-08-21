01945b99194aad922c9167cb63180930e92b7892
Beatnancio <AVenancio20000>
2022-08-21 12:18:38 +0100

    restrucutred execeution

--- src/main/scala/program/Program.scala
+++ src/main/scala/program/Program.scala
@@ -14,31 +14,32 @@ import java.io.File
 object Program {
   
   def analyse(project: Project[URL]): Unit = {
+    val code_analysis = new CodeAnalysis(project)
+
     val iterator = project.allProjectClassFiles.iterator
     while(iterator.hasNext) {
       val classFile = iterator.next
-      //println("―" * 50)
-      //println("[CLASSFILE]: " + classFile.thisType.toJava)
 
       //check fields
       classFile.fields.foreach(field => AndroidApiAnalysis.scan(field.fieldType.toJava, classFile.thisType.toJava))
 
       //check code
-      classFile.methodBodies.foreach(code => {
-        CodeAnalysis.scan(code, classFile.thisType.toJava)
-        AndroidApiAnalysis.scan(code, classFile.thisType.toJava)
+      classFile.methods.foreach(method => {
+        code_analysis.scan(method, classFile.thisType.toJava)
       })
-
     }
-    println(AndroidApiAnalysis.export.toString())
-    println(CodeAnalysis.export.toString())
   }
+/*
+println(AndroidApiAnalysis.export.toString())
+AndroidApiAnalysis.scan(code, classFile.thisType.toJava)
+println(CodeAnalysis.export.toString())
+*/
 
   def main(args: Array[String]): Unit = {
     println("―" * 50)
     
-    //open project
-    val jarPath = "mobsf-enjarify.jar"
+    //open hel
+    val jarPath = "basic.jar"
     implicit val project = Project(
       new java.io.File(jarPath), // path to the JAR files/directories containing the project
       org.opalj.bytecode.RTJar // predefined path(s) to the used libraries
