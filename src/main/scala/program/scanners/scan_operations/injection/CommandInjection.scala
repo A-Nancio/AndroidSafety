a507ab1e899103bdacc0e203684931472d7e600b
Beatnancio <AVenancio20000>
2022-09-15 01:22:51 +0100

    added injection scanOperations

--- src/main/scala/program/scanners/scan_operations/injection/CommandInjection.scala
+++ src/main/scala/program/scanners/scan_operations/injection/CommandInjection.scala
@@ -6,14 +6,52 @@ import org.opalj.ai.AIResult
 import org.opalj.ai.domain.l1.DefaultDomainWithCFGAndDefUse
 import java.net.URL
 import program.scanners.scan_operations.SecurityWarning
+import org.opalj.br.ObjectType
+import org.opalj.br.instructions.LoadString
 
 object CommandInjection extends ScanOperation {
   override def execute(methodCall: MethodInvocationInstruction, pc: Int, interpretation: AIResult{val domain: DefaultDomainWithCFGAndDefUse[URL]}): Boolean = {
-    return false
-  }
-  
-  
-  override def json: SecurityWarning = ???
+    val runtimeType = ObjectType("java/lang/Runtime")
+    if (methodCall.declaringClass == runtimeType && methodCall.name == "exec") {
+      val operands = interpretation.operandsArray(pc)
+    
+      val commandNameOrigin = interpretation.domain.origins(operands(0))
+      val runtimeOrigin = interpretation.domain.origins(operands(operands.size - 1))
 
-  override def name: String = ???
+      //NOTE: String array not covered
+      
+      interpretation.code.instructions(runtimeOrigin.head) match {
+        case instruction: MethodInvocationInstruction => 
+          if (instruction.declaringClass == runtimeType && instruction.name == "getRuntime") {
+            interpretation.code.instructions(commandNameOrigin.head) match {
+              case stringLoad: LoadString => return false // loading string implies loading always the same command
+              case _ => return true  // not constant can generate injections
+            }
+          }
+        case _ => return false
+      }
+    }
+    return false
+  } 
+  
+  override def json = SecurityWarning(
+    """User controlled strings in exec() will result in command execution.""",
+    "ERROR",
+    "cwe-78",
+    "m7",
+    "platform-2",
+    this.results,
+    "https://github.com/MobSF/owasp-mstg/blob/master/Document/0x04h-Testing-Code-Quality.md#injection-flaws-mstg-arch-2-and-mstg-platform-2"
+  )
+
+  override def name = "Command Injection"
 }
+
+/*
+patterns:
+      - pattern-not: 'Runtime.getRuntime().exec("...", ...);'
+      - pattern-not: 'Runtime.getRuntime().exec(new String[] {"...", ...}, ...);' -> Cannot check this one
+      - pattern-either:
+          - pattern: |
+              Runtime.getRuntime().exec(...);
+*/
--- src/main/scala/program/scanners/scan_operations/injection/SqLiteInjection.scala
+++ src/main/scala/program/scanners/scan_operations/injection/SqLiteInjection.scala
@@ -6,14 +6,58 @@ import org.opalj.ai.AIResult
 import org.opalj.ai.domain.l1.DefaultDomainWithCFGAndDefUse
 import java.net.URL
 import program.scanners.scan_operations.SecurityWarning
+import org.opalj.br.ObjectType
+import org.opalj.br.instructions.LoadString
 
 object SqLiteInjection extends ScanOperation {
   override def execute(methodCall: MethodInvocationInstruction, pc: Int, interpretation: AIResult{val domain: DefaultDomainWithCFGAndDefUse[URL]}): Boolean = {
+    val SQLiteDatabaseType = ObjectType("android/database/sqlite/SQLiteDatabase")
+    if (methodCall.declaringClass == SQLiteDatabaseType && (methodCall.name == "execSQL" || methodCall.name == "rawQuery")) {
+      val operands = interpretation.operandsArray(pc)
+      val firstArgumentOrigin = interpretation.domain.origins(operands(0))
+
+      interpretation.code.instructions(firstArgumentOrigin.head) match {
+        case stringLoad: LoadString => return false //loading a query from a raw string, it can not be manipulated
+        case _ => return true //anything else has a chance to be manipulated by the user
+      }
+    }
     return false
   }
   
-  override def json: SecurityWarning = ???
+  override def json = SecurityWarning(
+    """App uses SQLite Database and execute raw SQL query. Untrusted user input
+      in raw SQL queries can cause SQL Injection. Also sensitive information
+      should be encrypted and written to the database.""",
+    "WARNING",
+    "cwe-78",
+    "m7",
+    "platform-2",
+    this.results,
+    "https://github.com/MobSF/owasp-mstg/blob/master/Document/0x04h-Testing-Code-Quality.md#injection-flaws-mstg-arch-2-and-mstg-platform-2"
+  )
 
-  override def name: String = ???
+  override def name = "SQL Lite Injection"
 }
   
+/*
+patterns:
+      - pattern-not: '$DB.execSQL("..." , ...);'
+      - pattern-not: '$DB.rawQuery("..." , ...);'
+      - pattern-either:
+          - pattern: |
+              $DB.rawQuery("..." + $INP + "..." , ...);
+          - pattern: |
+              $DB.rawQuery($INP + "..." , ... );
+          - pattern: |
+              $DB.rawQuery($INP + "..." + $INP2, ...);
+          - pattern: |
+              $DB.rawQuery($INP + "..." + $INP2 + "...", ...);
+          - pattern: |
+              $DB.execSQL($INP + "..." , ...);
+          - pattern: |
+              $DB.execSQL("..." + $INP + "..." , ...);
+          - pattern: |
+              $DB.execSQL($INP + "..." + $INP2, ...);
+          - pattern: |
+              $DB.execSQL($INP + "..." + $INP2 + "...", ...);
+*/
